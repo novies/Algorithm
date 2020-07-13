8453aacb43db04b3baf42084be9f3fd70f9df5bb
novies <novem.IX.dev@gmail.com>
2020-07-13 09:31:08 +0900

    Level1

---
+++ Programmers/Level1/완주하지못한선수.py
@@ -0,0 +1,19 @@
+def solution(participant, completion):
+    participant = sorted(participant)
+    completion = sorted(completion)
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[-1]
+
+# 1
+participant = ["leo", "kiki", "eden"]
+completion = ["eden", "kiki"]
+
+# 2
+participant = ["marina", "josipa", "nikola", "vinko", "filipa"]
+completion = ["josipa", "filipa", "marina", "nikola"]
+
+# 3
+participant = ["mislav", "stanko", "mislav", "ana"]
+completion = ["stanko", "ana", "mislav"]
